b5d498b255e145e7b3cd88bcd27ecf2fc85858d3
Sophia Alfaro <scab06@icloud.com>
2023-06-23 12:43:31 -0600

    Initial Commit

---
+++ App with Interactive UI/App_with_Interactive_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_with_Interactive_UIApp.swift
+//  App with Interactive UI
+//
+//  Created by Sophia Cristina Alfaro Barillas on 23/6/23.
+//
+
+import SwiftUI
+
+@main
+struct App_with_Interactive_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
